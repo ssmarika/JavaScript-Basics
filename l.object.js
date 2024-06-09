ddc6939d1659ac7cd683535091631d8d6aa5f119
ssmarika <smarikashrestha02@gmail.com>
2024-06-09 16:30:14 +0545

    Initial Commit

---
+++ l.object.js
@@ -0,0 +1,11 @@
+// ! OBJECT
+
+// two approcah
+// 1. direct Object
+// 2. via class
+
+// key_value pair
+// collection of primary data type
+
+const studentDetail = {};
+console.log(studentDetail, typeof studentDetail);
